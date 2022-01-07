6a881388d9582356ec1d0a52c6340ae5ea7f6fb5
_StupidYou <91799709+Stupid4You@users.noreply.github.com>
2022-01-07 19:17:05 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,16 @@
-num1 = input("Enter num1: ")
-num2 = input("Enter num2: ")
-Operator = input("Enter operator * or / or - or +: ")
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+Operator = input("Enter operator * or / or - o +: ")
 
 def calculate():
   if Operator == "*":
-    return float(num1) * float(num2)
+    return num1 * num2
   elif Operator == "/":
-     return float(num1) / float(num2)
+     return num1 / num2
   elif Operator == "+":
-    return float(num1) + float(num2)
+    return num1 + num2
   else:
-    return float(num1) - float(num2)
+    return num1 - num2
 
 result = calculate()
 print(result)
