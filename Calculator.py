eab589002e205808afbc15ca969c2d8bf4fbdb5f
_Epic-Fish <91799709+Stupid4You@users.noreply.github.com>
2022-04-14 12:24:27 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
 num1 = float(input("Enter num1: "))
 num2 = float(input("Enter num2: "))
-Operator = input("Enter operator * or / or - o +: ")
+Operator = input("Enter operator * or / or - or +: ")
 
 def calculate():
   if Operator == "*":
