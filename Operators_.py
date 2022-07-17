5077648490c1691ec7c24959b27608bf949dc3b2
Just a pixel <91799709+ri-Soul@users.noreply.github.com>
2022-07-17 22:32:26 +0200

    just saving

--- Operators_.py
+++ Operators_.py
@@ -13,15 +13,24 @@ def LCM(num1, num2, num3):
     for x in range()
         Multiplied += num2
         multiplies2.append(Multiplied)
+    
+    for item in range(len(multiplies1)):
+        return multiplies1[item] == multiplies2[item]
 
 def GCD(num1, num2, num3):
 
-def SQRT(num1, num2, num3):
-    num1
-def AF_RF(num1, num2, num3):
-    num1
-def RF_AF(num1, num2, num3):
+def RAND(num1, num2, num3):
+    return random.randint(num1, num2)
 
+def MOD(num1, num2, num3):
+    return (num1/num2 - floor(num1/num2)) * num2
+
+def SQRT(num1, num2, num3):
+    return sqrt(num1)
+
+def AF_RF(num1, num2, num3):
+    
+def RF_AF(num1, num2, num3):
 
 def RF_AC(num1, num2, num3):
 
